a683ddbfeda9f86bcd30a4cfe7855125fe3f34eb
Maria Clara <mclarabarbosac@gmail.com>
2023-11-06 20:31:34 -0300

    iris tracking

--- main.py
+++ main.py
@@ -23,14 +23,49 @@ def get_blinking_ratio(eye_points, facial_landmarks):
     center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
     center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
 
-    hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 1)
-    ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 1)
+    #hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 1)
+    #ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 1)
 
     hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
     ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
 
     ratio = hor_line_lenght / ver_line_lenght
     return ratio
+
+def get_gaze_ratio(eye_points, facial_landmarks):
+    left_eye_region = np.array([(facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y),
+                                (facial_landmarks.part(eye_points[1]).x, facial_landmarks.part(eye_points[1]).y),
+                                (facial_landmarks.part(eye_points[2]).x, facial_landmarks.part(eye_points[2]).y),
+                                (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y),
+                                (facial_landmarks.part(eye_points[4]).x, facial_landmarks.part(eye_points[4]).y),
+                                (facial_landmarks.part(eye_points[5]).x, facial_landmarks.part(eye_points[5]).y)], np.int32)
+        
+    #cv2.polylines(frame, [left_eye_region], True, (0, 0, 255), 2)
+
+    height, width, _ = frame.shape
+    mask = np.zeros((height, width), np.uint8)
+
+    cv2.polylines(frame, [left_eye_region], True, 255, 2)
+    cv2.fillPoly(mask, [left_eye_region], 255)
+
+    eye = cv2.bitwise_and(gray, gray, mask=mask)
+
+    min_x = np.min(left_eye_region[:, 0])
+    max_x = np.max(left_eye_region[:, 0])
+    min_y = np.min(left_eye_region[:, 1])
+    max_y = np.max(left_eye_region[:, 1])
+
+    gray_eye = eye[min_y: max_y, min_x: max_x]
+    _, threshold_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY_INV)
+    height, width = threshold_eye.shape
+    left_side_threshold = threshold_eye[0: height, 0: int(width/2)]
+    left_side_white = cv2.countNonZero(left_side_threshold)
+
+    right_side_threshold = threshold_eye[0: height, int(width/2): width]
+    rigt_side_white = cv2.countNonZero(right_side_threshold)
+
+    gaze_ratio = left_side_white/rigt_side_white
+    return gaze_ratio
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -54,13 +89,31 @@ while True:
         #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
 
         landmarks = predictor(gray, face)
+
+        #detecção de piscadas
         left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
         right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
-        
+        blinking_ratio = (left_eye_ratio + right_eye_ratio)/2 
 
-        if  right_eye_ratio > 5.7:
-            cv2.putText(frame, "PISCANDO", (10, 100), font, 3, (255, 0, 0))
-        
+        if  blinking_ratio > 5.7:
+            cv2.putText(frame, "PISCANDO", (50, 400), font, 1, (255, 0, 0), 3)
+
+        #detecção da íris
+        gaze_ratio_left_eye = get_gaze_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        gaze_ratio_right_eye = get_gaze_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        gaze_ratio = (gaze_ratio_left_eye + gaze_ratio_right_eye)/2
+
+        #cv2.putText(frame, str(gaze_ratio), (50, 100), font, 2, (0, 0, 255), 3)
+
+        if gaze_ratio > 1.4:
+            cv2.putText(frame, "ESQUERDA", (50, 100), font, 1, (0, 0, 255), 3)
+        elif gaze_ratio < 0.6:
+            cv2.putText(frame, "DIREITA", (50, 100), font, 1, (0, 0, 255), 3)
+        elif 0.6 < gaze_ratio < 1.1:
+            cv2.putText(frame, "CENTRO", (50, 100), font, 1, (0, 0, 255), 3)
+        elif gaze_ratio > 1.1:
+            cv2.putText(frame, "CIMA", (50, 100), font, 1, (0, 0, 255), 3)
+
 
     # abre uma janela para a webcam no tamanho 640x360
     cv2.imshow("Camera", frame)
